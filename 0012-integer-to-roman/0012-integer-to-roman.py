aa3da6b087b8668eaf43ae23a43094ab488a1ea1
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-08-20 16:51:47 +0530

    Time: 3 ms (76.56%), Space: 19.5 MB (6.17%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,21 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        # Map values to their respective Roman numeral symbols in descending order
+        roman_mapping = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+        ]
+        
+        result = []
+        
+        # Loop through the values and greedy-subtract from num
+        for value, symbol in roman_mapping:
+            if num == 0:
+                break
+            count = num // value
+            if count:
+                result.append(symbol * count)
+                num %= value
+                
+        return "".join(result)
